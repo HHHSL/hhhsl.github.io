4fd32681e9f682fff72cd5332403ecacda3d9db4
HHHSL <2036802755@qq.com>
2021-05-05 22:50:51 +0800

    H

--- js/index.js
+++ js/index.js
@@ -1,19 +1,14 @@
 $(function () {
-   $(".Content_section").css("height", ($(window).height() - 56) + 'px');
-   setTimeout(function () { Carousel_content("Hi"); }, 1000)
-   setTimeout(function () { Carousel_content("别来无恙啊"); }, 3500)
-   setTimeout(function () { Carousel_content("这里是HHHSL"); }, 5500)
-   setTimeout(function () { Carousel_content("热爱生活"); }, 7500)
-   setTimeout(function () { Carousel_content("喜欢一切"); }, 9500)
-   setTimeout(function () { Carousel_content("外加亿点点帅"); }, 11500)
+    $("#subject .hi").css("height", ($(window).height() - 56) + 'px');
+    $("#subject .introduce").css("height", ($(window).height() - 56) + 'px');
+    setTimeout(function () {
+        $(".hi span").css("opacity","1")
+    }, 1500)
+    setTimeout(function () {
+        $(".hi").css("background-color","black");
+        $(".hi").css("color","white");
+    }, 2000)
 })
 
-// 轮播内容
-function Carousel_content(content) {
-   $(".Content_section").html("<h1 class='animate__animated animate__fadeInUp'> " + content + "</h1>")
-   setTimeout(function () {
-      $(".Content_section h1").removeAttr("class");
-      $(".Content_section").html("<h1 class='animate__animated animate__fadeOut animate__delay-0.8s'>" + content + "</h1>")
-   }, 1000)
-}
+
 
